f04c5ac661fb6d69125eeea7ab92b91cd027acb6
Anisovich_Maria <anisovich.masha@gmail.com>
2024-08-08 17:52:36 +0200

    feat: add background image to main screen

--- weather-forecast-brick/Controller/ViewController.swift
+++ weather-forecast-brick/Controller/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private lazy var backgroundImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "image_background"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        setupBackgroundImageView()
     }
 
+    private func setupBackgroundImageView() {
+        view.addSubview(backgroundImageView)
 
+        NSLayoutConstraint.activate([
+            backgroundImageView.topAnchor.constraint(equalTo: view.topAnchor),
+            backgroundImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            backgroundImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            backgroundImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
 }
-
